f54518869bb8e71dc2acacce4c41e50bb435430e
Filip Chalupa <chalupa.filip@gmail.com>
2020-06-21 11:39:11 +0200

    Dark, fullscreen

---
+++ src/components/FullScreenCountdown.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const FullScreenCountdown: React.SFC = (props) => (
+	<div className="fullScreenCountdown">{props.children}</div>
+)
--- src/components/Run.tsx
+++ src/components/Run.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Countdown } from './Countdown'
 import { useGetParameter } from '../utils/useQuery'
+import { FullScreenCountdown } from './FullScreenCountdown'
 
 export const Run: React.SFC = () => {
 	const [end, setEnd] = React.useState(new Date())
@@ -12,8 +13,8 @@ export const Run: React.SFC = () => {
 	}, [seconds])
 
 	return (
-		<>
+		<FullScreenCountdown>
 			<Countdown showHours end={end} />
-		</>
+		</FullScreenCountdown>
 	)
 }
