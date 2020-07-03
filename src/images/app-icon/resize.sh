3a2e9f0730bbd59325f1c674eac499f7bbfb460b
Filip Chalupa <chalupa.filip@gmail.com>
2020-07-03 20:02:35 +0200

    Icon update

--- src/images/app-icon/resize.sh
+++ src/images/app-icon/resize.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-original="favicon.svg"
+favicon="favicon.svg"
+original="original.svg"
 sizes=(36 48 72 96 128 144 192 256 384 512 1024 2048)
 
 for i in "${sizes[@]}"; do
 	echo Resizing $i
 	name=$i"x"$i".png"
+	inkscape.exe -z $favicon --export-width $i -e $name
+
+	name=$i"x"$i"-maskable.png"
 	inkscape.exe -z $original --export-width $i -e $name
 done
 
