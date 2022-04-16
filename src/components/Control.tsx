f627fed336957c041206947a18eb9b370dd685d0
Filip Chalupa <chalupa.filip@gmail.com>
2022-04-16 23:03:47 +0200

    Control: redirect to homepage if no id specified

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { useGetParameter } from '../utils/useQuery'
 import { ControlPanel } from './ControlPanel'
 
@@ -10,26 +10,5 @@ export const Control: React.FunctionComponent = () => {
 		return <ControlPanel id={id} />
 	}
 
-	return (
-		<ul>
-			{[
-				{
-					path: '/control/?id=1',
-					label: 'Room 1',
-				},
-				{
-					path: '/control/?id=2',
-					label: 'Room 2',
-				},
-				{
-					path: '/control/?id=3',
-					label: 'Room 3',
-				},
-			].map((item) => (
-				<li key={item.path}>
-					<Link to={item.path}>{item.label}</Link>
-				</li>
-			))}
-		</ul>
-	)
+	return <Redirect to="/" />
 }
