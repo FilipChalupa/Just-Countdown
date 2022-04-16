e39f2fbdca1daba5781bb3718b71fe234ccfa5bd
Filip Chalupa <chalupa.filip@gmail.com>
2022-04-16 20:56:15 +0200

    Frontpage: use Material UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,7 @@
+import { createTheme, useMediaQuery } from '@mui/material'
+import purple from '@mui/material/colors/purple'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import * as React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Control } from './Control'
@@ -6,14 +10,30 @@ import { Run } from './Run'
 import { Screen } from './Screen'
 
 export const App: React.FunctionComponent = () => {
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+	const theme = React.useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: prefersDarkMode ? 'dark' : 'light',
+					primary: purple,
+				},
+			}),
+		[prefersDarkMode],
+	)
+
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Frontpage} />
-				<Route exact path="/run/" component={Run} />
-				<Route exact path="/screen/" component={Screen} />
-				<Route exact path="/control/" component={Control} />
-			</Switch>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Frontpage} />
+					<Route exact path="/run/" component={Run} />
+					<Route exact path="/screen/" component={Screen} />
+					<Route exact path="/control/" component={Control} />
+				</Switch>
+			</Router>
+		</ThemeProvider>
 	)
 }
--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -1,67 +1,138 @@
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import SettingsIcon from '@mui/icons-material/Settings'
+import {
+	Box,
+	Button,
+	Container,
+	Grid,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	TextField,
+} from '@mui/material'
+import Typography from '@mui/material/Typography'
 import * as React from 'react'
-import { Link, useHistory } from 'react-router-dom'
+import { Link as RouterLink, useHistory } from 'react-router-dom'
 import logo from '../images/app-icon/favicon.svg'
 
 export const Frontpage: React.FunctionComponent = () => {
 	const history = useHistory()
-	const [customId, setCustomId] = React.useState('')
+	const [customId, setCustomId] = React.useState(() =>
+		Math.random().toString(36).slice(2, 10),
+	)
+
+	const staticPresets = React.useMemo(
+		() => [
+			{ to: '/run/?seconds=60', label: 'Countdown 1 minute' },
+			{ to: '/run/?seconds=120', label: 'Countdown 2 minutes' },
+			{ to: '/run/?seconds=300', label: 'Countdown 5 minutes' },
+			{ to: '/run/?seconds=600', label: 'Countdown 10 minutes' },
+			{ to: '/run/?seconds=3600', label: 'Countdown 60 minutes' },
+			{ to: '/run/?seconds=86400&showHours', label: 'Countdown 1 day' },
+		],
+		[],
+	)
+
+	const dynamicPresets = React.useMemo(() => ['1', '2', '3'], [])
 
 	return (
-		<div className="frontpage">
-			<div className="frontpage-in">
-				<h1 className="frontpage-title">
-					<img src={logo} alt="Logo" />
-					Just Countdown
-				</h1>
-				<h2>Presets</h2>
-				<ul>
-					<li>
-						<Link to="/run/?seconds=60">Countdown 1 minute</Link>
-					</li>
-					<li>
-						<Link to="/run/?seconds=120">Countdown 2 minutes</Link>
-					</li>
-					<li>
-						<Link to="/run/?seconds=300">Countdown 5 minutes</Link>
-					</li>
-					<li>
-						<Link to="/run/?seconds=600">Countdown 10 minutes</Link>
-					</li>
-					<li>
-						<Link to="/run/?seconds=3600">Countdown 60 minutes</Link>
-					</li>
-					<li>
-						<Link to="/run/?seconds=86400&amp;showHours">Countdown 1 day</Link>
-					</li>
-				</ul>
-				<h2>With remote control</h2>
-				<ol>
-					{[1, 2, 3].map((id) => (
-						<li key={id}>
-							<Link to={`/screen/?id=${id}`}>Screen</Link> /{' '}
-							<Link to={`/control/?id=${id}`}>Control</Link>
-						</li>
+		<Box paddingTop={4} paddingBottom={4}>
+			<Container maxWidth="xs">
+				<Box textAlign="center" paddingBottom={4}>
+					<img src={logo} alt="Logo" width="100" height="100" />
+					<Typography variant="h3" component="h1" gutterBottom>
+						Just Countdown
+					</Typography>
+					<Typography variant="body1">
+						Choose a countdown from presets below or use one with a remote
+						control.
+					</Typography>
+				</Box>
+				<Typography variant="h5" component="h2" gutterBottom>
+					Presets
+				</Typography>
+				<List>
+					{staticPresets.map((preset) => (
+						<ListItem key={preset.to} disablePadding>
+							<ListItemButton component={RouterLink} to={preset.to}>
+								<ListItemIcon>
+									<AccessTimeIcon />
+								</ListItemIcon>
+								<ListItemText primary={preset.label} />
+							</ListItemButton>
+						</ListItem>
 					))}
-					<li>
-						<form
-							onSubmit={(event) => {
-								event.preventDefault()
-								history.push(`/control/?id=${encodeURIComponent(customId)}`)
-							}}
+				</List>
+				<Box paddingBottom={1} />
+				<Typography variant="h5" component="h2" gutterBottom>
+					With remote control
+				</Typography>
+				<List>
+					{dynamicPresets.map((presetId) => (
+						<ListItem
+							key={presetId}
+							disablePadding
+							secondaryAction={
+								<IconButton
+									component={RouterLink}
+									to={`/control/?id=${presetId}`}
+								>
+									<SettingsIcon />
+								</IconButton>
+							}
 						>
-							Custom <br />
-							<input
-								placeholder="id"
+							<ListItemButton
+								component={RouterLink}
+								to={`/screen/?id=${presetId}`}
+							>
+								<ListItemIcon>
+									<AccessTimeIcon />
+								</ListItemIcon>
+								<ListItemText primary={`Show countdown ${presetId}`} />
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
+				<Box paddingBottom={1} />
+				<Typography variant="h5" component="h2" gutterBottom>
+					Create custom controlled
+				</Typography>
+				<form
+					onSubmit={(event) => {
+						event.preventDefault()
+						history.push(`/control/?id=${encodeURIComponent(customId)}`)
+					}}
+				>
+					<Box paddingBottom={0.5} />
+					<Grid container alignItems="center" spacing={1}>
+						<Grid item xs={8}>
+							<TextField
+								fullWidth
+								size="small"
+								label="Choose your id"
+								variant="outlined"
 								required
 								value={customId}
 								onChange={(event) => setCustomId(event.target.value)}
 							/>
-							<button type="submit">create</button>
-						</form>
-					</li>
-				</ol>
-				{/* @TODO: More <Link to="/control/">Control</Link> */}
-			</div>
-		</div>
+						</Grid>
+						<Grid item xs={4}>
+							<Button
+								variant="contained"
+								type="submit"
+								size="large"
+								fullWidth
+								endIcon={<SettingsIcon />}
+							>
+								Open
+							</Button>
+						</Grid>
+					</Grid>
+				</form>
+			</Container>
+		</Box>
 	)
 }
