25dbafff96de7349cd51e371d0f1764f3e476f4f
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-09 15:35:17 +0100

    Webmanifest: add categories

--- src/webmanifest.ts
+++ src/webmanifest.ts
@@ -3,6 +3,7 @@ export const start_url = '/'
 export const description = 'Countdown timer with remote control.'
 export const background_color = '#ffffff'
 export const theme_color = '#000000'
+export const categories = ['productivity', 'utilities']
 
 const sizes = [36, 48, 72, 96, 128, 144, 192, 256, 384, 512, 1024, 2048]
 export const icons = [
