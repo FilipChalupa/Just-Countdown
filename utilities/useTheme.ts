ea5c9ea614b8aca0abaa25b0a1d9061d8645cbca
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-12 20:00:40 +0100

    Use theme: prevent white flash in dark theme

--- utilities/useTheme.ts
+++ utilities/useTheme.ts
@@ -2,7 +2,9 @@ import { createTheme, PaletteColorOptions, useMediaQuery } from '@mui/material'
 import * as React from 'react'
 
 export const useTheme = (color: PaletteColorOptions) => {
-	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', {
+		defaultMatches: true, // Prevents white flash in dark theme
+	})
 
 	const theme = React.useMemo(
 		() =>
