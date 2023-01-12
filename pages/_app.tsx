e0b3d47df6369f5afbf2d985c9b21661728b7a87
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-12 20:38:07 +0100

    Fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { purple } from '@mui/material/colors'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 import { registerServiceWorker } from '../utilities/registerServiceWorker'
 import { useTheme } from '../utilities/useTheme'
@@ -13,6 +14,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<ThemeProvider theme={theme}>
+			<Head>
+				<title>Just Countdown</title>
+				<meta
+					name="description"
+					content="Countdown timer with remote control."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<CssBaseline />
 			<Component {...pageProps} />
 		</ThemeProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,15 @@ export default function Document() {
 					media="(prefers-color-scheme: dark)"
 					content="#121212"
 				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
 			</Head>
 			<body>
 				<Main />
